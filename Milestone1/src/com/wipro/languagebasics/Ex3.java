cf6259ea006450c7c923da0b95499a351f989c73
Personal <Personal@SUMANTH>
2020-06-14 21:56:23 +0530

    version 1

---
+++ Milestone1/src/com/wipro/languagebasics/Ex3.java
@@ -0,0 +1,10 @@
+package com.wipro.languagebasics;
+
+public class Ex3 {
+	public static void main(String[] args) {
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		System.out.println("The sum of "+a+" and "+b+" is "+(a+b));
+	}
+
+}
